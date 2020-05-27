c76e7d7660bc5e8343ed3e39aa2648dc201ad49b
jeanpiereche <jeanpiereche@gmail.com>
2020-05-26 22:05:38 -0500

    fe ww ew

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         TextBox1.Text = "Hola mundo wefwefew few!"
+        TextBox1.Text = "Hola mundo wefwefew few!"
+        TextBox1.Text = "Hola mundo wefwefew few!"
     End Sub
 End Class
